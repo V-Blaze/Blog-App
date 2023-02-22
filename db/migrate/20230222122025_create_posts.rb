0eb207aef25744f494bb5a5f1234a03e7f115973
V-Blaze <valentine6586@gmail.com>
2023-02-22 14:36:25 +0100

    Update foreign key refernce from user_id to author_id

--- db/migrate/20230222122025_create_posts.rb
+++ db/migrate/20230222122025_create_posts.rb
@@ -5,7 +5,7 @@ class CreatePosts < ActiveRecord::Migration[7.0]
       t.text :text
       t.integer :comments_counter
       t.integer :likes_counter
-      t.references :user, null: false, foreign_key: true
+      t.references :author, foreign_key: { to_table: 'users' }
 
       t.timestamps
     end
