18dfb02d7eb32d104873ecf451c34dd5fff519c7
V-Blaze <valentine6586@gmail.com>
2023-03-01 10:16:51 +0100

    Add test for the posts controller

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -2,6 +2,28 @@ require 'rails_helper'
 
 RSpec.describe 'Posts', type: :request do
   describe 'GET /index' do
-    pending "add some examples (or delete) #{__FILE__}"
+    before(:example) { get '/users/:user_id/posts' } # get(:index)
+    it 'is a success' do
+      expect(response).to have_http_status(:ok)
+    end
+    it "renders 'index' template" do
+      expect(response).to render_template(:index)
+    end
+    it 'Includes the correct placeholder text' do
+      expect(response.body).to include('Here is a list of all the posts from this user')
+    end
+  end
+  describe 'GET /show' do
+    before(:example) { get '/users/:user_id/posts/:post_id' }
+
+    it 'is a success' do
+      expect(response).to have_http_status(:ok)
+    end
+    it "renders 'index' template" do
+      expect(response).to render_template(:show)
+    end
+    it 'Includes the correct placeholder text' do
+      expect(response.body).to include('Here is the details about this post from this user')
+    end
   end
 end
