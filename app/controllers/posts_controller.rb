98219868d020cdef90230e716958675720ef371a
V-Blaze <valentine6586@gmail.com>
2023-03-02 18:25:05 +0100

    Add form and functionality to create a new post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,4 +6,23 @@ class PostsController < ApplicationController
   def show
     @post = User.find(params[:user_id]).posts.find(params[:id])
   end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(
+      author: current_user,
+      title: params[:post][:title],
+      text: params[:post][:text]
+    )
+    
+    if @post.save
+      redirect_to users_path
+    else
+      render :new
+    end
+  end
+
 end
