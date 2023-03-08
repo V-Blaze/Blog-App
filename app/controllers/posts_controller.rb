5bc736350b1f645e2d0e66d867cd4004db577092
V-Blaze <valentine6586@gmail.com>
2023-03-08 08:52:55 +0100

    Fix N+1 querry problem using Includes

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
   def index
-    @user = User.includes(:posts).find(params[:user_id])
+    @user = User.includes(:posts, :comments).find(params[:user_id])
   end
 
   def show
